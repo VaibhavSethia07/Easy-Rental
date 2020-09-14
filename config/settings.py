9ef4dd52d26613010e9796b5f362dbcc96364191
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2020-09-14 19:33:54 +0530

    Configure user uploads to seen on click

--- config/settings.py
+++ config/settings.py
@@ -135,4 +135,6 @@ STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"uploads")
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+MEDIA_URL = "/media/"
