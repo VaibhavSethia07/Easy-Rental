d07cb95b46004344c5321b8509868abf8b2f6d8a
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2020-09-20 21:22:42 +0530

    Create page list navigation

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from datetime import datetime
 from django.shortcuts import render
 from . import models
@@ -8,8 +9,14 @@ def all_rooms(request):
     limit = page * page_size
     offset = limit - page_size
     rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    
     return render(request, "rooms/home.html", {
-        "room": rooms
+        "room": rooms,
+        "page": page,
+        "page_count": page_count,
+        "page_range": range(1,page_count+1)
+        
     })
     
     
