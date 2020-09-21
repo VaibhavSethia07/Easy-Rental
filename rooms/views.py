13e4b030e35c306b766fdcf86393c62c70533919
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2020-09-21 22:15:04 +0530

    Add Django pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,35 @@
 from math import ceil
 from datetime import datetime
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 def all_rooms(request):
     page = int(request.GET.get("page",1))
-    page_size = 10
-    limit = page * page_size
-    offset = limit - page_size
-    rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+
     return render(request, "rooms/home.html", {
-        "room": rooms,
-        "page": page,
-        "page_count": page_count,
-        "page_range": range(1,page_count+1)
-        
+        "rooms": rooms,
     })
+
+
+# def all_rooms(request):
+    # page = int(request.GET.get("page",1))
+    # page_size = 10
+    # limit = page * page_size
+    # offset = limit - page_size
+    # rooms = models.Room.objects.all()[offset:limit]
+    # page_count = ceil(models.Room.objects.count() / page_size)
+    # 
+    # return render(request, "rooms/home.html", {
+        # "room": rooms,
+        # "page": page,
+        # "page_count": page_count,
+        # "page_range": range(1,page_count+1)
+        # 
+    # })
     
     
     
